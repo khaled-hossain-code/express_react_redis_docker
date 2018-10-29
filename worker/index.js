df5a08802fea6f8777b16fe7280cbfc0f81ee80d
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-10-29 18:09:57 +0600

    fix worker side typo

--- worker/index.js
+++ worker/index.js
@@ -1,26 +1,20 @@
-const redis = require('redis');
 const keys = require('./keys');
+const redis = require('redis');
 
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
-  retry_strategy: () => 1000 // try to reconnect every 1second
+  retry_strategy: () => 1000
 });
-
 const sub = redisClient.duplicate();
 
-function fib(index){
-  if(index < 2) {
-    return 1;
-  }
-  
-  return fib(index - 1) + fib(index - 2)
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-// watch redis anytime a new message comes
 sub.on('message', (channel, message) => {
+  console.log('setting the value');
   redisClient.hset('values', message, fib(parseInt(message)));
 });
-
-// Subscribes the client to the specified channels.
 sub.subscribe('insert');
