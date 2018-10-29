a90ca1e0e5489388003eccac0da99b58047a5b13
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-10-29 18:10:34 +0600

    fix server side typo

--- server/index.js
+++ server/index.js
@@ -1,13 +1,11 @@
 const keys = require('./keys');
 
-// express setup
+// Express App Setup
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const redis = require('redis');
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
 
@@ -20,24 +18,23 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort
 });
+pgClient.on('error', () => console.log('Lost PG connection'));
 
-pgClient.on('error', () => {
-  console.log('lost pg connection');
-});
-
-pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
+pgClient
+  .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
 
-// redis client setup
+// Redis Client Setup
+const redis = require('redis');
 const redisClient = redis.createClient({
   host: keys.redisHost,
   port: keys.redisPort,
   retry_strategy: () => 1000
 });
-
 const redisPublisher = redisClient.duplicate();
 
-// express route handler
+// Express route handlers
+
 app.get('/', (req, res) => {
   res.send('Hi');
 });
@@ -51,24 +48,23 @@ app.get('/values/all', async (req, res) => {
 app.get('/values/current', async (req, res) => {
   redisClient.hgetall('values', (err, values) => {
     res.send(values);
-  })
+  });
 });
 
 app.post('/values', async (req, res) => {
   const index = req.body.index;
 
-  if(parseInt(index) > 40) {
+  if (parseInt(index) > 40) {
     return res.status(422).send('Index too high');
   }
 
-  redisClient.hset('values', index, '');
+  redisClient.hset('values', index, 'Nothing yet!');
   redisPublisher.publish('insert', index);
-
   pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-  res.send({ working: true});
+  res.send({ working: true });
 });
 
-app.listen(5000, err => {
-  console.log('listening on port 5000');
+app.listen(8000, err => {
+  console.log('Listening');
 });
